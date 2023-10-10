a8afe6799a1dd70274e6be1d3a5831302ea051a5
JimmyIstWeicher <143940482+JimmyIstWeicher@users.noreply.github.com>
2023-10-10 20:30:02 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { WebSocketServer } = require("ws");
-const sockserver = new WebSocketServer({ port: 3000 });
+const sockserver = new WebSocketServer({ port: 300 });
 let firstID = "";
 let secondID = "";
 let myInterval;
